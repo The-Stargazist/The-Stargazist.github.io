9de3bf187ec014e35badc8b93ca9f93579f3f4c1
Nanda Kishore <fizikastardom@gmail.com>
2024-11-30 01:00:14 -0800

    srsly

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'the-stargazist.github.io',
+  site: 'The-Stargazist.github.io',
   base: '/',
   trailingSlash: 'always',
   integrations: [
